f2b75743517095f93066123c277741cef5df4d6e
jpimentel <jpimentel@visit.uaveiro.eu>
2020-08-20 18:08:19 +0100

    SplashScreen done

--- app/src/main/java/com/joaopimentel/shoppinglistapp/SplashActivity.java
+++ app/src/main/java/com/joaopimentel/shoppinglistapp/SplashActivity.java
@@ -5,20 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView shoppingCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        shoppingCart = findViewById(R.id.shopping_cart);
+
+        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadein);
+        shoppingCart.startAnimation(animFadeIn);
+
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                showMainActivity();
-            }
-        }, 2000);
+        handler.postDelayed(this::showMainActivity, 3000);
     }
 
     private void showMainActivity() {
