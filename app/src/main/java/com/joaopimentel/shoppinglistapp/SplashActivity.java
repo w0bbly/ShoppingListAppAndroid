a20179559db517a56562a41de17d136563de23bf
jpimentel <jpimentel@visit.uaveiro.eu>
2020-08-21 17:41:03 +0100

    Login screen started

--- app/src/main/java/com/joaopimentel/shoppinglistapp/SplashActivity.java
+++ app/src/main/java/com/joaopimentel/shoppinglistapp/SplashActivity.java
@@ -2,27 +2,26 @@ package com.joaopimentel.shoppinglistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     private ImageView shoppingCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-        shoppingCart = findViewById(R.id.shopping_cart);
-
-        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadein);
-        shoppingCart.startAnimation(animFadeIn);
 
         Handler handler = new Handler();
         handler.postDelayed(this::showMainActivity, 3000);
